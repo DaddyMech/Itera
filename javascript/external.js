2ec31a2f9e4fc29a7fed03b0c0f7c1bbfc82e839
technoe <technoe@gmail.com>
2015-09-21 19:11:53 -0500

    css, index, js changes
    added css tags and properties
    fixed js time stamp
    new tags

--- javascript/external.js
+++ javascript/external.js
@@ -2,9 +2,9 @@ function updateTime() {
     var d = new Date();
     var d1 = new Date().toDateString();
     var t1 = d.getHours();
-    var t2 = d.getMinutes();
-    var t3 = d.getSeconds();
-    document.getElementById("datetime").innerHTML = (d1 + "\n" + t1 + ":" + t2 + "\n");
+    var t2 = ("0" + d.getMinutes()).substr(-2);
+    var t3 = ("0" + d.getSeconds()).substr(-2);
+    document.getElementById("datetime").innerHTML = ("The date and time is: " + d1 + "\n" + t1 + ":" + t2 + ":" + t3 + "\n");
 
     setTimeout(updateTime,1000);
 }
