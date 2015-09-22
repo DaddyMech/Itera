912c52f4ddd66b19ebcdd1b1a3cae695c1a49dc8
technoe <technoe@gmail.com>
2015-09-21 19:15:01 -0500

    formatting
    just updating formatting

--- javascript/external.js
+++ javascript/external.js
@@ -1,10 +1,9 @@
 function updateTime() {
     var d = new Date();
     var d1 = new Date().toDateString();
-    var t1 = d.getHours();
+    var t1 = ("0" + d.getHours()).substr(-2);
     var t2 = ("0" + d.getMinutes()).substr(-2);
     var t3 = ("0" + d.getSeconds()).substr(-2);
     document.getElementById("datetime").innerHTML = ("The date and time is: " + d1 + "\n" + t1 + ":" + t2 + ":" + t3 + "\n");
-
     setTimeout(updateTime,1000);
 }
